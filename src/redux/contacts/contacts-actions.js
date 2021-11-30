59bba3a8c2fdae7c2d205a03eaa8cef716784825
AndriiPotap <andrew.potapenko47@gmail.com>
2021-12-01 01:50:49 +0200

    final push

---
+++ src/redux/contacts/contacts-actions.js
@@ -0,0 +1,40 @@
+// import { ADD, DELETE, CHANGE_FILTER } from './contacts-types';
+import { createAction } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
+const addContacts = createAction('contacts/add', ({ name, number }) => {
+  return {
+    payload: {
+      id: uuidv4(),
+      name,
+      number,
+    },
+  };
+});
+
+const deleteContacts = createAction('contacts/delete');
+const changeFilter = createAction('contacts/changeFitler');
+
+const contactsActions = { addContacts, deleteContacts, changeFilter };
+
+export default contactsActions;
+
+
+// const addContacts = ({ name, number }) => ({
+//   type: ADD,
+//   payload: {
+//     id: uuidv4(),
+//     name,
+//     number,
+//   },
+// });
+
+// const deleteContacts = contactId => ({
+//   type: DELETE,
+//   payload: contactId,
+// });
+
+// const changeFilter = value => ({
+//   type: CHANGE_FILTER,
+//   payload: value,
+// });
---
+++ src/redux/store.js
@@ -0,0 +1,54 @@
+// import { createStore, combineReducers } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
+import contactsReducer from './contacts/contacts-reducer';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+// const rootReducer = combineReducers({
+//   contacts: persistReducer(persistConfig, contactsReducer),
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const persistor = persistStore(store);
+
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+// });
+
+// const store = createStore(rootReducer, composeWithDevTools());
